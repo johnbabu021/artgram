7075d12e27bfab687952bc1ddc952fbc2a7ccb6b
johnbabu021 <johnbabu021@gmail.com>
2022-06-28 22:07:07 +0530

    chors:posts design

--- src/components/dashboard/middlepostcomponent.jsx
+++ src/components/dashboard/middlepostcomponent.jsx
@@ -1,3 +1,25 @@
+import '../../styles/dashboard/posts.css'
+
 export default function MiddlePostComponent() {
-  return <div>Middlepost</div>;
+  return(
+     <section className="middle__posts">
+<div  className="create__posts">
+  <div  className='post__inputs'>
+<img src="logo512.png"/>
+<input  placeholder='share your thoughts '/>
+  </div>
+  <div  className='upload__types'>
+    <div  className='photo'>
+    <img src="pht.svg"/>  photo
+    </div>
+    <div  className='post__submit'>
+      <button>post</button>
+    </div>
+
+  </div>
+  </div>
+
+
+
+  </section>);
 }
